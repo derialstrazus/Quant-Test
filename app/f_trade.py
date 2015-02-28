ebdfa7a497c76f13eca5d580be420dd13903fbc2
BenedictLee <lee.2138@buckeyemail.osu.edu>
2015-02-28 11:44:14 -0600

    Create trade file and check TempData floder existence

--- app/f_trade.py
+++ app/f_trade.py
@@ -1,7 +1,28 @@
 
-def MACDTrading(quotes, bank, name, newYearIndex, resultsFile):
+def MACDTrading(quotes, trigger, start, end):
     shares = 0
-    sharesBrought = 0
-    startingBank = bank
-    totalBank = bank
-    resultsFile.write('')
+    sharesValue = 0
+    capital = 10000
+    netWorth = capital + sharesValue
+    for n in range(start, end):
+        indicator = quotes[trigger][n]
+        if indicator == 1:                                  #Strong buy; Commit all money
+            sharesBought = int(capital/quotes.AdjClose[n])  #Number of share to buy
+            sharesCost = sharesBought * quotes.AdjClose[n]  #Amount spend
+            shares = shares + sharesBought                  #Number of shares brought
+            netWorth = netWorth - sharesCost
+        elif indicator == 0:
+            pass
+        else:
+            sharesQuantity = int(shares)
+            shares = shares - sharesQuantity
+            if shares != 0:
+                print 'something wrong!'
+            sharesSold = sharesQuantity * quotes.AdjClose[n]
+            netWorth = netWorth + sharesSold
+
+    performance = 100.0 * netWorth/capital
+    return netWorth
+
+
+
